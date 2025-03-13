7d591e9c70718416c81b8322125803c569f886a0
maryfrx7 <marycarmenflores007@outlook.com>
2025-03-13 09:18:53 -0600

    Firts: Login

---
+++ ControlSistem/src/archivos/LoginController.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package archivos;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Mary Flores
+ */
+public class LoginController implements Initializable {
+    @FXML
+    private Button close;
+    @FXML
+    private Button loginBtn;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private TextField username;
+    
+    @FXML
+    void close(ActionEvent event) {
+        System.exit(0);
+    }
+    @Override
+       public void initialize(URL url, ResourceBundle rb) {
+    }    
+    
+    void login(ActionEvent event){
+        
+    }
+    
+}
